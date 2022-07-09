e2e64790b94205a5c0afdfa97117a41f1ca1a7a6
Hikari <50797982+Natural-Harmonia-Gropius@users.noreply.github.com>
2022-07-09 12:15:42 +0000

    update

---
+++ src/App.component.ts
@@ -0,0 +1,29 @@
+/* eslint-disable max-classes-per-file */
+import { Component, Pipe, PipeTransform } from "@angular/core";
+
+const TRAFFIC_LIGHTS = ["red", "orange", "green"];
+
+@Pipe({
+  name: "light",
+})
+export class TrafficLightPipe implements PipeTransform {
+  transform(value: number): string {
+    return TRAFFIC_LIGHTS[value];
+  }
+}
+
+@Component({
+  selector: "app-trafficlight",
+  templateUrl: "./trafficlight.component.html",
+})
+export class TrafficlightComponent {
+  lightIndex: number = 0;
+
+  nextLight(): void {
+    if (this.lightIndex + 1 > TRAFFIC_LIGHTS.length - 1) {
+      this.lightIndex = 0;
+    } else {
+      this.lightIndex += 1;
+    }
+  }
+}
